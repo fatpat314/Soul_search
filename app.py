3836c3e8e5b801e9a738622240e38fcabc8c4a86
Patrick <w.patrick.kelly@gmail.com>
2019-12-03 19:43:47 -0800

    trying to add mongod

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Soul_search
+souls = db.souls
 
 app = Flask(__name__)
 
-souls = [
-    { 'name': 'James Brown', 'price': '$100' },
-    { 'name': 'John Coltrain', 'price': '$1000' }
-]
+# souls = [
+#     { 'name': 'James Brown', 'price': '$100' },
+#     { 'name': 'John Coltrain', 'price': '$1000' }
+# ]
 
 @app.route('/')
 def index():
     """Return homepage"""
-    return render_template('index.html', souls=souls)
+    return render_template('index.html', souls=souls.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
