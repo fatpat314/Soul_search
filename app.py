ae9f1f7ca62b3be5b04f3489fdde5ef8d028948d
Patrick <w.patrick.kelly@gmail.com>
2019-12-03 19:33:28 -0800

    Souls for sale

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app = Flask(__name__)
+souls = [
+    { 'name': 'James Brown', 'price': '$100' },
+    { 'name': 'John Coltrain', 'price': '$1000' }
+]
 
 @app.route('/')
 def index():
     """Return homepage"""
-    return render_template('home.html', msg='Flask is Cool')
+    return render_template('index.html', souls=souls)
 
 if __name__ == '__main__':
     app.run(debug=True)
