531f4b4cc9cdc98ec44cb57b223fd05edf3856cd
Patrick <w.patrick.kelly@gmail.com>
2019-12-05 14:15:11 -0800

    Souls now have avatars, however its not creating new images

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson import ObjectId
 
 client = MongoClient()
-db = client.Soul_search
-souls = db.souls
+db = client.soul_search
+souls_collection = db.souls
 
 app = Flask(__name__)
 
-def picture_url_creator(id_lst):
-    souls = []
-    for soul_id in id_lst:
-        soul = 'https://www.google.com/' + soul_id
-        souls.append(soul)
-    return souls
+# def picture_url_creator(id_lst):
+#     souls = []
+#     for soul_id in id_lst:
+#         soul = 'https://www.google.com/' + soul_id
+#         souls.append(soul)
+#     return souls
 
 # souls = [
 #     { 'name': 'James Brown', 'price': '$100' },
@@ -22,7 +23,7 @@ def picture_url_creator(id_lst):
 @app.route('/')
 def index():
     """Return homepage"""
-    return render_template('index.html', souls=souls.find())
+    return render_template('index.html', souls=souls_collection.find())
 
 @app.route('/souls/new')
 def souls_new():
@@ -32,16 +33,18 @@ def souls_new():
 @app.route('/souls', methods=['POST'])
 def souls_submit():
     """Submit a soul"""
-    soul_ids = request.form.get('soul_ids').split()
-
-    souls = picture_url_creator(soul_ids)
-
-    souls = {
+    soul = {
         'name': request.form.get('name'),
         'price': request.form.get('price')
     }
-    print(request.form.to_dict())
-    return redirect(url_for('index'))
+    souls_collection.insert(soul)
+    return redirect('/')
+
+@app.route('/souls/<soul_id>')
+def souls_show(soul_id):
+    """Show a single soul"""
+    soul = souls_collection.find_one({'_id': ObjectId(soul_id)})
+    return render_template('souls_show.html', soul=soul)
 
 if __name__ == '__main__':
     app.run(debug=True)
