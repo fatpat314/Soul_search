d935a2991834b7fbd46ee9396fd10b5614dfa713
Patrick <w.patrick.kelly@gmail.com>
2019-12-10 19:25:52 -0800

    heroku is now working

--- tests.py
+++ tests.py
@@ -40,14 +40,21 @@ class SoulsTest(TestCase):
         result = self.client.get(f'/souls/{sample_soul_id}')
         self.assertEqual(result.status, '200 OK')
 
-    # @mock.patch('pymongo.collection.Collection.update_one')
-    # def test_update_soul(self, mock_update):
-    #     result = self.client.post(f'/souls/{sample_soul_id}', data=sample_form_data)
+
+'''What am I not understanding about these tests?'''
+    # # @mock.patch('pymongo.collection.Collection.update_one')
+    # # def test_update_soul(self, mock_update):
+    # #     result = self.client.post(f'/souls/{sample_soul_id}', data=sample_form_data)
+    # #
+    # #     self.assertEqual(result.status, '302 FOUND')
+    # #     mock_update.assert_call_with({'_id': sample_soul_id}, {'$set': sample_soul})
     #
+    # @mock.patch('pymongo.collection.Collection.delete_one')
+    # def test_delete_soul(self, mock_delete):
+    #     form_data = {'_method': 'DELETE'}
+    #     result = self.client.post(f'/souls/{sample_soul_id}/delete', data=sample_form_data)
     #     self.assertEqual(result.status, '302 FOUND')
-    #     mock_update.assert_call_with({'_id': sample_soul_id}, {'$set': sample_soul})
-
-
+    #     mock_delete.assert_call_with({'_id': sample_soul_id})
 
 
 
